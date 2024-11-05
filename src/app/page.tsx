963b4c3fcb695fc6e152427f1421deda5df8e8d1
cchi852 <chris.chiem01@gmail.com>
2024-11-06 03:02:42 +1300

    bigtext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
     return (
         <div className="flex flex-col justify-center items-center h-dvh">
-            <h1 className="font-bold text-3xl">Chris Chiem</h1>
+            <h1 className="font-bold text-[5rem] text-[#04061D]">
+                Chris Chiem
+            </h1>
             <p className="text-lg font-inter">📍 Auckland, New Zealand</p>
         </div>
     );
