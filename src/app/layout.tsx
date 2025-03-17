fabdfa52bdc7f55f3d7a095a83339930d90bf8b7
cchi852 <chris.chiem01@gmail.com>
2025-03-18 00:26:03 +1300

    basic info, grid and color blur bg added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,20 +3,20 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
-    title: "Chris Chiem - Portfolio",
-    description: "Portfolio",
+  title: "Chris Chiem - Portfolio",
+  description: "Portfolio",
 };
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={`${inter.className} antialiased`}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,81 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image'
+import React from 'react'
 
 const Home = () => {
+    const gridWidth = 50
+    const gridColor = 'blue-200'
     return (
-        <div className="flex flex-col justify-center items-center h-dvh w-dvw">
-            <h1 className="font-bold text-[5rem] text-[#04061D]">
-                Chris Chiem
-            </h1>
-            <Image
-                src="/assets/gif/tardis.gif"
-                alt="Tardis"
-                width={100}
-                height={100}
-            />
-            <Image
-                src="/assets/icons/nextjs.svg"
-                alt="NextJs Icon"
-                width={50}
-                height={50}
-            />
-            <p className="text-lg font-inter">📍 Auckland, New Zealand</p>
+        <div className="flex h-dvh w-dvw flex-col items-center justify-center">
+            <div className="relative max-w-3xl">
+                <h1 className="text-3xl font-bold text-[#04061D]">
+                    Hi, I&apos;m Chris 👋
+                </h1>
+                {/* <svg
+                    className={`absolute inset-0 -z-1 size-full stroke-${gridColor}`}
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <defs>
+                        <pattern
+                            id="grid"
+                            width={gridWidth}
+                            height={gridWidth}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <path
+                                d={`M ${gridWidth} 0 L ${gridWidth} ${gridWidth}`}
+                                strokeWidth="1"
+                            />
+                            <path
+                                d={`M 0 ${gridWidth} L ${gridWidth} ${gridWidth}`}
+                                strokeWidth="1"
+                            />
+                        </pattern>
+                    </defs>
+                    <rect width="100%" height="100%" fill="url(#grid)" />
+                </svg> */}
+                <div className="absolute top-0 left-1/3 size-72 -translate-x-1/2 rounded-full bg-purple-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+                <div className="absolute top-0 left-1/2 size-72 -translate-x-1/2 rounded-full bg-pink-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+                <div className="absolute top-6 left-2/3 size-72 -translate-x-1/2 rounded-full bg-yellow-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+                <div className="">
+                    <h2 className="font-bold">About Me</h2>
+                    <p className="text-gray-600">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dolores repudiandae
+                        <span className="underline">
+                            earum tenetur omnis modi
+                        </span>
+                        , atque exercitationem quisquam,
+                        <span className="underline">
+                            earum tenetur omnis modi
+                        </span>
+                        obcaecati similique, delectus molestias at quos sunt
+                        quam sint rerum.
+                        <span className="underline">
+                            earum tenetur omnis modi
+                        </span>
+                        , voluptatum nostrum quos nulla at rem harum!
+                    </p>
+                </div>
+                <div>
+                    <Image
+                        src="/assets/gif/tardis.gif"
+                        alt="Tardis"
+                        width={100}
+                        height={100}
+                    />
+                    <Image
+                        src="/assets/icons/nextjs.svg"
+                        alt="NextJs Icon"
+                        width={50}
+                        height={50}
+                    />
+                </div>
+                <p className="font-inter mt-5 ml-5 flex text-lg">
+                    📍 Auckland, New Zealand
+                </p>
+            </div>
         </div>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home
