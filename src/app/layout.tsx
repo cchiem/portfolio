765ff4788f8b66786a02754a97fa4eb43e3fad69
cchi852 <chris.chiem01@gmail.com>
2025-03-18 01:29:57 +1300

    dot background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
+import './globals.css'
+import { Gabarito } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "Chris Chiem - Portfolio",
-  description: "Portfolio",
-};
+    title: 'Chris Chiem - Portfolio',
+    description: 'Portfolio',
+}
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Gabarito({
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+})
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,48 @@ const Home = () => {
     // const gridWidth = 50
     // const gridColor = 'blue-200'
     return (
-        <div className="flex h-dvh w-dvw flex-col items-center justify-center">
-            <div className="relative max-w-3xl">
-                <h1 className="text-3xl font-bold text-[#04061D]">
-                    Hi, I&apos;m Chris 👋
-                </h1>
-                {/* <svg
-                    className={`absolute inset-0 -z-1 size-full stroke-${gridColor}`}
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <defs>
-                        <pattern
-                            id="grid"
-                            width={gridWidth}
-                            height={gridWidth}
-                            patternUnits="userSpaceOnUse"
-                        >
-                            <path
-                                d={`M ${gridWidth} 0 L ${gridWidth} ${gridWidth}`}
-                                strokeWidth="1"
-                            />
-                            <path
-                                d={`M 0 ${gridWidth} L ${gridWidth} ${gridWidth}`}
-                                strokeWidth="1"
-                            />
-                        </pattern>
-                    </defs>
-                    <rect width="100%" height="100%" fill="url(#grid)" />
-                </svg> */}
+        <div className="relative flex min-h-screen min-w-full flex-col items-center justify-center bg-[url('/assets/background/subtle-dots.png')] bg-[size:40px] bg-top bg-repeat">
+            {/* <svg
+                className={`absolute inset-0 -z-1 size-full stroke-${gridColor}`}
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <defs>
+                    <pattern
+                        id="grid"
+                        width={gridWidth}
+                        height={gridWidth}
+                        patternUnits="userSpaceOnUse"
+                    >
+                        <path
+                            d={`M ${gridWidth} 0 L ${gridWidth} ${gridWidth}`}
+                            strokeWidth="1"
+                        />
+                        <path
+                            d={`M 0 ${gridWidth} L ${gridWidth} ${gridWidth}`}
+                            strokeWidth="1"
+                        />
+                    </pattern>
+                </defs>
+                <rect width="100%" height="100%" fill="url(#grid)" />
+            </svg> */}
+            <Image
+                src="/assets/gif/tardis.gif"
+                alt="Tardis"
+                width={100}
+                height={100}
+                className="absolute top-20 left-20"
+                unoptimized={true}
+            />
+            <div className="relative max-w-3xl flex-col gap-4">
+                <div className="flex flex-col">
+                    <h1 className="text-3xl font-bold text-[#04061D]">
+                        Hi, I&apos;m Chris 👋
+                    </h1>
+                    <h2>Software Developer & Student @ UoA</h2>
+                    <p className="">📍 Auckland, New Zealand</p>
+                </div>
+
+                {/* BG COLORS */}
                 <div className="absolute top-0 left-1/3 size-72 -translate-x-1/2 rounded-full bg-purple-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
                 <div className="absolute top-0 left-1/2 size-72 -translate-x-1/2 rounded-full bg-pink-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
                 <div className="absolute top-6 left-2/3 size-72 -translate-x-1/2 rounded-full bg-yellow-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
@@ -56,23 +70,19 @@ const Home = () => {
                         , voluptatum nostrum quos nulla at rem harum!
                     </p>
                 </div>
+
+                {/* Skills */}
                 <div>
-                    <Image
-                        src="/assets/gif/tardis.gif"
-                        alt="Tardis"
-                        width={100}
-                        height={100}
-                    />
-                    <Image
-                        src="/assets/icons/nextjs.svg"
-                        alt="NextJs Icon"
-                        width={50}
-                        height={50}
-                    />
+                    <div className="flex flex-col items-center gap-1">
+                        <Image
+                            src="/assets/icons/nextjs.svg"
+                            alt="NextJs Icon"
+                            width={50}
+                            height={50}
+                        />
+                        <p>Nextjs</p>
+                    </div>
                 </div>
-                <p className="font-inter mt-5 ml-5 flex text-lg">
-                    📍 Auckland, New Zealand
-                </p>
             </div>
         </div>
     )
