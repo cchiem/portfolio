47792c2c533075f7425bda9f75cf6829b45553b2
cchi852 <chris.chiem01@gmail.com>
2025-03-18 03:58:27 +1300

    patch

--- src/app/components/Skill.tsx
+++ src/app/components/Skill.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 
-const Skill = (name: string, source: string) => {
+const Skill = ({ name, source }: { name: string; source: string }) => {
     return (
         <div className="flex flex-col items-center gap-1">
             <Image src={source} alt={name} width={50} height={50} />
